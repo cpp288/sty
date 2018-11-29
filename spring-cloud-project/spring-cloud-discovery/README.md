7e4ed066ad7fec4daba9c3ada78ffb436a72b703
chenjian <chenjian097@qq.com>
2018-11-29 10:21:12 +0800

    增加spring cloud discovery zookeeper

--- spring-cloud-project/spring-cloud-discovery/README.md
+++ spring-cloud-project/spring-cloud-discovery/README.md
@@ -16,3 +16,34 @@ spring cloud 服务注册中心
 | 适用规模      | 20 K ~ 30 K 实例（节点）                                | 10K ~ 20K 实例（节点）       | < 3K 实例（节点）         |
 | 性能问题      | 简单的更新机制、复杂设计、规模较大时 GC 频繁            | 扩容麻烦、规模较大时 GC 频繁 | 3K 节点以上，更新列表缓慢 |
 
+## zookeeper
+### 注意点
+引用依赖的时候，由于原先依赖的是zookeeper3.5.x，这个版本是beta版本，需要排除引用3.4.x，spring官网有说明
+```xml
+<dependency>
+    <groupId>org.springframework.cloud</groupId>
+    <artifactId>spring-cloud-starter-zookeeper-all</artifactId>
+    <exclusions>
+        <exclusion>
+            <groupId>org.apache.zookeeper</groupId>
+            <artifactId>zookeeper</artifactId>
+        </exclusion>
+    </exclusions>
+</dependency>
+<dependency>
+    <groupId>org.apache.zookeeper</groupId>
+    <artifactId>zookeeper</artifactId>
+    <version>3.4.12</version>
+    <exclusions>
+        <exclusion>
+            <groupId>org.slf4j</groupId>
+            <artifactId>slf4j-log4j12</artifactId>
+        </exclusion>
+    </exclusions>
+</dependency>
+```
+
+需要使用bootstrap.properties，不能使用application.properties（亲测不行），参考[官网](https://cloud.spring.io/spring-cloud-zookeeper/single/spring-cloud-zookeeper.html)
+
+相关博客：
+1. [使用Spring Cloud Zookeeper + Feign实现服务发现](https://www.cnblogs.com/karascanvas/p/7521942.html)
---
+++ spring-cloud-project/spring-cloud-discovery/spring-cloud-zookeeper/src/main/java/com/cpp/cloud/discovery/zookeeper/controller/ServiceController.java
@@ -0,0 +1,48 @@
+package com.cpp.cloud.discovery.zookeeper.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 测试
+ *
+ * @author chenjian
+ * @date 2018-11-29 10:17
+ */
+@RestController
+public class ServiceController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 返回所有的服务名称
+     *
+     * @return
+     */
+    @GetMapping("/services")
+    public List<String> getAllServices() {
+        return discoveryClient.getServices();
+    }
+
+    /**
+     * 获取实例的名称-ip：port
+     *
+     * @param serviceName
+     * @return
+     */
+    @GetMapping("/service/instances/{serviceName}")
+    public List<String> getAllServiceInstances(@PathVariable String serviceName) {
+        return discoveryClient.getInstances(serviceName)
+                .stream()
+                .map(s ->
+                        s.getServiceId() + " - " + s.getHost() + ":" + s.getPort()
+                ).collect(Collectors.toList());
+    }
+}

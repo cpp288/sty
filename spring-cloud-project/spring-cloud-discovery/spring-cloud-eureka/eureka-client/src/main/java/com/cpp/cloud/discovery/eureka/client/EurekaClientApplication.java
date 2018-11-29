4841834d6b5fa5c2b7372b1465b313a591cd71e0
chenjian <chenjian097@qq.com>
2018-11-29 13:55:32 +0800

    增加spring cloud eureka client

---
+++ spring-cloud-project/spring-cloud-discovery/spring-cloud-eureka/eureka-client/src/main/java/com/cpp/cloud/discovery/eureka/client/EurekaClientApplication.java
@@ -0,0 +1,20 @@
+package com.cpp.cloud.discovery.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 启动类
+ *
+ * @author chenjian
+ * @date 2018-11-29 10:41
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}

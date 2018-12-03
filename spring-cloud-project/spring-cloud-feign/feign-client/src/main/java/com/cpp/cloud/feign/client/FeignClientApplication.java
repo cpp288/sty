4683fbda9a7531cfd106cb6ac6bfc8bc579c7eda
chenjian <chenjian097@qq.com>
2018-12-03 11:06:35 +0800

    增加feign

---
+++ spring-cloud-project/spring-cloud-feign/feign-client/src/main/java/com/cpp/cloud/feign/client/FeignClientApplication.java
@@ -0,0 +1,21 @@
+package com.cpp.cloud.feign.client;
+
+import com.cpp.cloud.feign.server.api.ServerApi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * 启动类
+ *
+ * @author chenjian
+ * @date 2018-12-03 10:02
+ */
+@SpringBootApplication
+@EnableFeignClients(clients = ServerApi.class)
+public class FeignClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignClientApplication.class, args);
+    }
+}

8cc82f10a1a42293a3d69af7bc66c29c09887ced
chenjian <chenjian097@qq.com>
2018-12-03 11:31:31 +0800

    u

--- spring-cloud-project/spring-cloud-feign/feign-client/src/main/java/com/cpp/cloud/feign/client/FeignClientApplication.java
+++ spring-cloud-project/spring-cloud-feign/feign-client/src/main/java/com/cpp/cloud/feign/client/FeignClientApplication.java
@@ -3,6 +3,7 @@ package com.cpp.cloud.feign.client;
 import com.cpp.cloud.feign.server.api.ServerApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients(clients = ServerApi.class)
+@EnableDiscoveryClient
 public class FeignClientApplication {
 
     public static void main(String[] args) {

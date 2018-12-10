5bc8eae2094c24a7cfefc1fb61658b3f94276b33
chenjian <chenjian097@qq.com>
2018-12-10 21:55:14 +0800

    u

--- spring-cloud-project/spring-cloud-stream/README.md
+++ spring-cloud-project/spring-cloud-stream/README.md
@@ -10,7 +10,7 @@
 3. https://stackoverflow.com/questions/43778809/how-to-manage-manual-ack-with-spring-cloud-stream-rabbit
 4. https://www.cnblogs.com/yangzhilong/p/7904461.html
 
-# 为什么需要SpringCloud Stream消息驱动呢
+## 为什么需要SpringCloud Stream消息驱动呢
 比方说我们用到了RabbitMQ和Kafka，由于这两个消息中间件的架构上的不同，像RabbitMQ有exchange，kafka有Topic，partitions分区，这些中间件的差异性导致我们实际项目开发给我们造成了一定的困扰，我们如果用了两个消息队列的其中一种，后面的业务需求，我想往另外一种消息队列进行迁移，这时候无疑就是一个灾难性的，一大堆东西都要重新推倒重新做，因为它跟我们的系统耦合了，这时候springcloud Stream给我们提供了一种解耦合的方式。
 
 如下是官方文档提供的架构图所示：
@@ -21,6 +21,6 @@ Spring Cloud Stream由一个中间件中立的核组成。应用通过Spring Clo
 
 通道通过指定中间件的Binder实现与外部代理连接。业务开发者不再关注具体消息中间件，只需关注Binder对应用程序提供的抽象概念来使用消息中间件实现业务即可。
 
-# 各类stream类比
+## 各类stream类比
 
 ![stream](image/stream.png)
--- spring-cloud-project/spring-cloud-stream/sink-samples/src/main/java/com/cpp/cloud/stream/sink/samples/ReceiveService.java
+++ spring-cloud-project/spring-cloud-stream/sink-samples/src/main/java/com/cpp/cloud/stream/sink/samples/ReceiveService.java
@@ -1,19 +1,33 @@
 package com.cpp.cloud.stream.sink.samples;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.SubscribableChannel;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.UnsupportedEncodingException;
 
 /**
  * 接收服务
+ * 在接收的时候可以使用3中方式：
+ *  1. 基于接口编程的方式 init()
+ *  2. 使用 Spring cloud stream 注解驱动（最终也是基于 Spring Integration 实现的）
+ *  3. 使用 Spring Integration 注解驱动
+ * 以上这3种方式不能同时接收到消息
  *
  * @author chenjian
  * @date 2018-11-28 15:12
  */
 @Service
 public class ReceiveService {
+
+    @Autowired
+    private MySink mySink;
 
     /**
      * 默认 sink
@@ -26,7 +40,25 @@ public class ReceiveService {
     }
 
     /**
-     * 自定义 sink
+     * 基于接口编程
+     */
+    @PostConstruct
+    public void init() {
+        SubscribableChannel subscribableChannel = mySink.myInput();
+        subscribableChannel.subscribe(message -> {
+            MessageHeaders headers = message.getHeaders();
+            String encoding = (String) headers.get("charset-encoding");
+            byte[] content = (byte[]) message.getPayload();
+            try {
+                System.out.println("接收到消息：" + new String(content, encoding));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    /**
+     * 使用 Spring cloud stream 注解驱动
      *
      * @param msg
      */
@@ -37,5 +69,15 @@ public class ReceiveService {
         // 获取消息头
         MessageHeaders headers = msg.getHeaders();
         System.out.printf("[mysink]接收到消息：%s\n", message);
+    }
+
+    /**
+     * 使用 Spring Integration 注解驱动
+     *
+     * @param message
+     */
+    @ServiceActivator(inputChannel = MySink.INPUT)
+    public void onServiceActivator(String message) {
+        System.out.printf("[onServiceActivator]接收到消息：%s\n", message);
     }
 }

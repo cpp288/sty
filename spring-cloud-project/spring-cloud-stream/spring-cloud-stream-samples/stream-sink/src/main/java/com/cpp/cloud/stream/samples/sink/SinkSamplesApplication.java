be4756702111bcd3a698556f71c59df75f710cf1
chenjian <chenjian097@qq.com>
2018-12-11 15:01:44 +0800

    u

--- spring-cloud-project/spring-cloud-stream/spring-cloud-stream-samples/stream-sink/src/main/java/com/cpp/cloud/stream/samples/sink/SinkSamplesApplication.java
+++ spring-cloud-project/spring-cloud-stream/spring-cloud-stream-samples/stream-sink/src/main/java/com/cpp/cloud/stream/samples/sink/SinkSamplesApplication.java
@@ -7,9 +7,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.SubscribableChannel;
 import org.springframework.messaging.handler.annotation.SendTo;
+
+import javax.annotation.PostConstruct;
+import java.io.UnsupportedEncodingException;
 
 /**
  * 启动类
@@ -31,34 +36,43 @@ public class SinkSamplesApplication {
     /**
      * 基于接口编程
      */
-//    @PostConstruct
-//    public void init() {
-//        SubscribableChannel subscribableChannel = messageService.test();
-//        subscribableChannel.subscribe(message -> {
-//            MessageHeaders headers = message.getHeaders();
-//            String encoding = (String) headers.get("charset-encoding");
-//            byte[] content = (byte[]) message.getPayload();
-//            try {
-//                System.out.println("接收到消息：" + new String(content, encoding));
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
-//        });
-//    }
+    @PostConstruct
+    public void init() {
+        SubscribableChannel subscribableChannel = messageService.testInput();
+        subscribableChannel.subscribe(message -> {
+            MessageHeaders headers = message.getHeaders();
+            String encoding = (String) headers.get("charset-encoding");
+            byte[] content = (byte[]) message.getPayload();
+            try {
+                System.out.println("接收到消息：" + new String(content, encoding));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 
     /**
      * 使用 Spring cloud stream 注解驱动
      *
      * @param msg
      */
-//    @StreamListener(MessageService.TEST_INPUT)
-//    public void receiveMyInput(Message<String> msg) {
-//        // 获取消息体
-//        String message = msg.getPayload();
-//        // 获取消息头
-//        MessageHeaders headers = msg.getHeaders();
-//        System.out.printf("[test]接收到消息：%s\n", message);
-//    }
+    @StreamListener(MessageService.TEST_INPUT)
+    public void receiveMyInput(Message<MessageData> msg) {
+        // 获取消息体
+        MessageData message = msg.getPayload();
+        // 获取消息头
+        MessageHeaders headers = msg.getHeaders();
+        System.out.printf("[test]接收到消息：%s\n", message);
+    }
+
+    @StreamListener(MessageService.TEST_INPUT)
+    public void receiveMyInput2(Message<MessageData> msg) {
+        // 获取消息体
+        MessageData message = msg.getPayload();
+        // 获取消息头
+        MessageHeaders headers = msg.getHeaders();
+        System.out.printf("[test2]接收到消息：%s\n", message);
+    }
 
     /**
      * 反馈消息
@@ -66,20 +80,20 @@ public class SinkSamplesApplication {
      * @param msg
      * @return
      */
-    @StreamListener(MessageService.TEST_INPUT)
-    @SendTo(MessageService.TEST_OUTPUT)
-    public String sendToReceive(Message<MessageData> msg) {
-        System.out.printf("[test]接收到消息：%s\n", msg.getPayload());
-        return msg.getPayload() + " received";
-    }
+//    @StreamListener(MessageService.TEST_INPUT)
+//    @SendTo(MessageService.TEST_OUTPUT)
+//    public String sendToReceive(Message<MessageData> msg) {
+//        System.out.printf("[test]接收到消息：%s\n", msg.getPayload());
+//        return msg.getPayload() + " received";
+//    }
 
     /**
      * 使用 Spring Integration 注解驱动
      *
      * @param message
      */
-//    @ServiceActivator(inputChannel = MessageService.TEST_INPUT)
-//    public void onServiceActivator(String message) {
-//        System.out.printf("[onServiceActivator]接收到消息：%s\n", message);
-//    }
+    @ServiceActivator(inputChannel = MessageService.TEST_INPUT)
+    public void onServiceActivator(MessageData message) {
+        System.out.printf("[onServiceActivator]接收到消息：%s\n", message);
+    }
 }
